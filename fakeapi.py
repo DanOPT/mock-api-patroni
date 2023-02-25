50b2a03d5d6b1e7076ff9a9e92dd897b70dd0ae1
Daniel Patrick <daniel.patrick@netways.de>
2023-02-25 19:05:58 +0100

    Improved pylint

--- fakeapi.py
+++ fakeapi.py
@@ -1,8 +1,12 @@
+
+"""
+fakeapi.py - Emulates Patroni endpoints
+"""
 #from datetime import datetime, timezone
+import json
+import uvicorn
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
-import uvicorn
-import json
 
 app = FastAPI()
 
@@ -21,60 +25,55 @@ class FailoverRequest(BaseModel):
     candidate: str
     scheduled_at: str
 
-# TODO verification if this is the right request and response
 @app.get("/patroni")
-async def read_json():
-    with open('json/endpoint_patroni.json', 'r') as f:
-        data = json.load(f)
+async def patroni_endpoint():
+    with open('json/endpoint_patroni.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
     return data
 
-# TODO verification if this is the right request and response
 @app.get("/cluster")
-async def read_json():
-    with open('json/endpoint_cluster.json', 'r') as f:
-        data = json.load(f)
+async def cluster_endpoint():
+    with open('json/endpoint_cluster.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
     return data
 
-# TODO verification if this is the right request and response
 @app.get("/history")
-async def read_json():
-    with open('json/endpoint_history.json', 'r') as f:
-        data = json.load(f)
+async def history_endpoint():
+    with open('json/endpoint_history.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
     return data
 
-# TODO verification if this is the right request and response
 @app.get("/config")
-async def read_json():
-    with open('json/endpoint_config.json', 'r') as f:
-        data = json.load(f)
+async def config_endpoint():
+    with open('json/endpoint_config.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
     return data
 
-# TODO verification if this is the right request and response
 @app.post("/reload")
-async def reload_config():
+async def reload_endpoint():
     return Response(content="Successfully scheduled reload", status_code=202)
 
-# TODO verification if this is the right request and response
 @app.post("/restart")
-async def reload_config():
+async def restart_endpoint():
     return Response(content="Successfully scheduled restart", status_code=202)
 
-# TODO verification if this is the right request and response
-# curl -s http://localhost:8888/switchover -XPOST -d '{"leader":"postgresql0","scheduled_at":"2019-09-24T12:00+00"}' -H "Content-Type: application/json"
+# curl -s http://localhost:8888/switchover -XPOST  \
+# -d '{"leader":"postgresql0","scheduled_at":"2019-09-24T12:00+00"}' \
+# -H "Content-Type: application/json"
 @app.post("/switchover")
-async def perform_switchover(request: SwitchoverRequest):
+async def switchover_endpoint(request: SwitchoverRequest):
     leader = request.leader
     scheduled_at = request.scheduled_at
     # perform the switchover logic here
     return {"message": f"Switching over to {leader} at {scheduled_at}."}
 
-# TODO verification if this is the right request and response
-# curl -s http://localhost:8888/failover -XPOST -d '{"candidate":"postgresql12","scheduled_at":"2019-09-25T12:00+00"}' -H "Content-Type: application/json"
+# curl -s http://localhost:8888/failover -XPOST \
+# -d '{"candidate":"postgresql12","scheduled_at":"2019-09-25T12:00+00"}' \
+# -H "Content-Type: application/json"
 @app.post("/failover")
-async def perform_failover(request: FailoverRequest):
+async def failover_endpoint(request: FailoverRequest):
     candidate = request.candidate
     scheduled_at = request.scheduled_at
-    # perform the failover logic here
     return {"message": f"Failing over to {candidate} at {scheduled_at}."}
 
 if __name__ == "__main__":
